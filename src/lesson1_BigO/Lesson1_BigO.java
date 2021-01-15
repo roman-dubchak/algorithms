2461e5878ece338243f8e36c3183ca8fd300f2fd
Roman Dubchak <dubrathers@mail.ru>
2021-01-15 09:22:39 +0300

    upd project

--- src/lesson1_BigO/Lesson1_BigO.java
+++ src/lesson1_BigO/Lesson1_BigO.java
@@ -1,6 +1,5 @@
 package lesson1_BigO;
 
-import java.sql.Array;
 import java.util.Arrays;
 
 public class Lesson1_BigO {
@@ -8,14 +7,12 @@ public class Lesson1_BigO {
 
 //        TODO:
 //        1. Описать простейшие алгоритмы
-
 //        1.1. Возведение в степень *используя чётность степени*
-        int[] a = new int[]{2, 7, 3, 1, 4, 4};
-        System.out.println("Before Exponentiation " + Arrays.toString(a));
-        arrExponentiation(a);
-        System.out.println("Before Exponentiation " + Arrays.toString(a));
+        System.out.println("Exponentiation:  " + еxponentiation(2, 3));
 
 //        1.2. Поиск минимального элемента в массиве
+        int[] a = new int[]{2, 7, 3, 1, 4, 4};
+        System.out.println("Arrays: " + Arrays.toString(a));
         arrMin(a);
         System.out.println("Minimum array value " + arrMin(a));
 
@@ -25,15 +22,20 @@ public class Lesson1_BigO {
 //        2. Подсчитать сложность описанных алгоритмов
 //        3. Какие правила подсчёта применялись, оставьте комментарии в коде
     }
-    public static void arrExponentiation(int[] arr){
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] *= arr[i];
+
+    // Сложность O(n)
+    private static int еxponentiation(int base, int exponent){
+        for (int i = 1; i < exponent; i++) {
+            base *= base;
         }
+        return base;
     }
 
-    public static int arrMin(int[] arr){
+    // Сложность O(n)
+    // По замыслу хотел сравнить первый элемент массива с остальными, но не успел к началу занятия
+    // Поулчилось, что бегу по массиву 2 раза
+    private static int arrMin(int[] arr){
         int min = 0;
-        int halfArr = arr.length / 2;
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j < arr.length - 1; j++) {
                 if (arr[i] < arr[j]) {
@@ -44,12 +46,12 @@ public class Lesson1_BigO {
         return min;
     }
 
-    public static float arrAver (int[] arr){
-        int av = 0;
+    // Сложность O(n)
+    private static float arrAver (int[] arr){
+        float av = 0;
         for (int i = 0; i < arr.length; i++) {
             av += arr[i];
         }
-        return (float) av / arr.length;
+        return av / arr.length;
     }
-
 }
